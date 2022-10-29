d9d37d8a3f10e755cb7090c8e9a5ded5de9ec14c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-29 16:00:06 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState(0)
@@ -33,8 +33,49 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     }, [])
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = "absolute"
+    const background = "indigo"
+    const size = Math.min(w, h) / 10 
+    return {
+        barStyle() : CSSProperties {
+            const width = `${size / 3}px`
+            const height = `${size}px`
+            const top = `${h - h * 0.5 * sinify(scale)}px`
+            const left = `${w / 2 - size / 6}px`
+            return {
+                background, 
+                position, 
+                width, 
+                height, 
+                top, 
+                left
+            }
+        },
+        dotStyle() : CSSProperties {
+            const width = `${size / 5}px`
+            const height = `${size / 5}px`
+            const left = `${(w / 2 - size / 2) * sinify(scale)}px`
+            const top = `${h / 2 - size / 5}px`
+            const position = 'absolute'
+            const borderRadius = `50%`
+            return {
+                width, 
+                height, 
+                position, 
+                left, 
+                top, 
+                background, 
+                borderRadius 
+            }
+        }
+    }
 }
